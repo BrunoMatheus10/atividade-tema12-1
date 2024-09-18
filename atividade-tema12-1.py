d1cfe9a1acb594ac5403a707409483780c735f88
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-18 16:31:49 -0300

    Add files via upload

---
+++ atividade-tema12-1.py
@@ -0,0 +1,26 @@
+# Par 1: 10 e 2
+A = 10  # Inicializa a variável A com o valor 10
+B = 2   # Inicializa a variável B com o valor 2
+C = 0   # Inicializa a variável C com o valor 0
+while A > B:  # Enquanto A for maior que B, execute o loop
+    A = A - B  # Subtrai B de A e armazena o resultado em A
+    C += 1     # Incrementa C em 1
+print("Para (10, 2): C =", C, "A =", A)  # Imprime os valores finais de C e A
+
+# Par 2: 6 e 2
+A = 6  # Inicializa a variável A com o valor 6
+B = 2  # Inicializa a variável B com o valor 2
+C = 0  # Inicializa a variável C com o valor 0
+while A > B:  # Enquanto A for maior que B, execute o loop
+    A = A - B  # Subtrai B de A e armazena o resultado em A
+    C += 1     # Incrementa C em 1
+print("Para (6, 2): C =", C, "A =", A)  # Imprime os valores finais de C e A
+
+# Par 3: 15 e 3
+A = 15  # Inicializa a variável A com o valor 15
+B = 3   # Inicializa a variável B com o valor 3
+C = 0   # Inicializa a variável C com o valor 0
+while A > B:  # Enquanto A for maior que B, execute o loop
+    A = A - B  # Subtrai B de A e armazena o resultado em A
+    C += 1     # Incrementa C em 1
+print("Para (15, 3): C =", C, "A =", A)  # Imprime os valores finais de C e A
